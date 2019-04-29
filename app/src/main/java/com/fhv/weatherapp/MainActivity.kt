652e0f3c3b59083f9fcbde49f0789d95a5b1e7f2
Piotr Pawluk <vynaloze@gmail.com>
2019-04-29 11:14:14 +0200

    Update location on request in the background

--- app/src/main/java/com/fhv/weatherapp/MainActivity.kt
+++ app/src/main/java/com/fhv/weatherapp/MainActivity.kt
@@ -37,17 +37,13 @@ import com.fhv.weatherapp.model.CurrentLocation
 import com.fhv.weatherapp.service.notification.network.NetworkBroadcastReceiver
 import com.fhv.weatherapp.service.weatherupdater.ForecastUpdater
 import com.fhv.weatherapp.viewmodel.WeatherViewModel
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
-import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.IOException
+import kotlinx.android.synthetic.main.list_view.*
 import java.util.*
 import javax.xml.validation.Validator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var locationManager: LocationManager
     private var mDrawer: DrawerLayout? = null
     private var toolbar: Toolbar? = null
     private var navigationView: NavigationView? = null
@@ -70,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         SharedPrefs.initializeSharedPreferences(this)
 
         if (Common.cityList.isEmpty()) {
-            getLocationListener()
+            askForPermissionsAndUpdateWeather()
         } else {
             ForecastUpdater.startInBackground()
         }
@@ -128,8 +124,8 @@ class MainActivity : AppCompatActivity() {
                     info.text = weather.toString()
                 })*/
 
-        /* setting listener for get location button
-        btn_get_location.setOnClickListener { getLocationListener() }*/
+        // setting listener for get location button
+        getWeatherButton.setOnClickListener { askForPermissionsAndUpdateWeather() }
 
         toolbar = findViewById(R.id.toolbar) as Toolbar
         val toolbarTitle = findViewById(R.id.toolbar_title) as TextView
@@ -191,11 +187,11 @@ class MainActivity : AppCompatActivity() {
 
 
     // check if user allowed to use location services
-    private fun getLocationListener() {
+    private fun askForPermissionsAndUpdateWeather() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), Common.PERMISSION_REQUEST_ACCESS_COARSE_LOCATION)
         } else {
-            getLocation()
+            ForecastUpdater.updateOnce()
         }
     }
 
@@ -205,146 +201,11 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             Common.PERMISSION_REQUEST_ACCESS_COARSE_LOCATION -> {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getLocation()
+                    ForecastUpdater.updateOnce()
                 } else {
                     Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-    }
-
-    // adding location update listeners and get location data
-    @SuppressLint("MissingPermission")
-    private fun getLocation() {
-
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0F, object : LocationListener {
-                override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-
-                override fun onProviderEnabled(provider: String?) {}
-
-                override fun onProviderDisabled(provider: String?) {}
-
-                override fun onLocationChanged(location: Location?) {
-                    if (location != null) {
-
-                        val curloc = getLocationResult(location)
-
-                        if (curloc != null) {
-
-                            var including = false
-
-                            for (city in Common.cityList) {
-                                if (city.location.city == curloc.city) {
-                                    Common.lastCityIndex = Common.cityList.indexOf(city)
-                                    including = true
-                                    break
-                                }
-                            }
-
-                            if (!including){
-                                Common.cityList.add(City(null, curloc))
-                                Common.lastCityIndex = Common.cityList.size - 1
-                            }
-
-                            ForecastUpdater.updateOnce()
-
-                            locationManager.removeUpdates(this)
-                        }
-                    }
-                }
-            })
-
-            val localNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-
-            if (localNetworkLocation != null) {
-                val location: Location = localNetworkLocation
-                getLocationResult(location)
-
-                val curloc = getLocationResult(location)
-
-                if (curloc != null) {
-
-                    var including = false
-
-                    for (city in Common.cityList) {
-                        if (city.location.city == curloc.city) {
-                            Common.lastCityIndex = Common.cityList.indexOf(city)
-                            including = true
-                            break
-                        }
-                    }
-
-                    if (!including){
-                        Common.cityList.add(City(null, curloc))
-                        Common.lastCityIndex = Common.cityList.size - 1
-                    }
-
-                    ForecastUpdater.updateOnce()
-                }
-            }
-        } else {
-
-            val builder = LocationSettingsRequest.Builder().addLocationRequest(LocationRequest())
-            val client: SettingsClient = LocationServices.getSettingsClient(this)
-            val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
-
-            task.addOnSuccessListener { getLocationListener() }
-
-            task.addOnFailureListener { exception ->
-                if (exception is ResolvableApiException) {
-                    try {
-                        exception.startResolutionForResult(this@MainActivity,
-                                Common.REQUEST_CHECK_SETTINGS)
-                    } catch (sendEx: IntentSender.SendIntentException) {
-                        // Ignore the error.
-                    }
-                }
-            }
-        }
-    }
-
-    // print data as Toast and return CurrentLocation data object
-    private fun getLocationResult(location: Location): CurrentLocation? {
-        try {
-            val city: String = getCityName(location.latitude, location.longitude)
-            Toast.makeText(this, city + "\n lat: " + location.latitude +
-                    "\n lng: " + location.longitude, Toast.LENGTH_SHORT).show()
-            return CurrentLocation(city, location.latitude, location.longitude)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Toast.makeText(this, "City name was not found", Toast.LENGTH_SHORT).show()
-        }
-        return null
-    }
-
-    // get city name from coordinates
-    private fun getCityName(lat: Double, lng: Double): String {
-        val geocoder = Geocoder(this, Locale.getDefault())
-        val addresses: List<Address>
-
-        try {
-            addresses = geocoder.getFromLocation(lat, lng, 10)
-            if (addresses.isNotEmpty()) {
-                for (address in addresses) {
-                    if (address.locality != null && address.locality.isNotEmpty()) {
-                        return address.locality
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        Log.d(Common.APP_NAME, "City name was not found.")
-
-        return ""
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(broadcastReceiver)
     }
 }
